11678d2c58888a32807ec3869cca712d3fa21a7a
Joao Jacques <joao.jacques@yumu.ai>
2024-08-05 18:55:47 -0300

    update 5 files

--- src/components/log-first-view-duration.tsx
+++ src/components/log-first-view-duration.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import {useIntersectionObserver} from "usehooks-ts";
+// import {useIntersectionObserver} from "usehooks-ts";
 import {useEffect, useState} from "react";
 import {logEvent} from "firebase/analytics";
 import {clientAnalytics} from "@/lib/firebase/client-app";
@@ -12,44 +12,44 @@ interface Props {
 export default function LogFirstViewDuration(props: Props) {
   const [isLogged, setIsLogged] = useState(false);
   const [viewStartTime, setViewStartTime] = useState<Date | null>(null);
-  const {isIntersecting, ref} = useIntersectionObserver({
-    threshold: 0.5,
-  });
+  // const {isIntersecting, ref} = useIntersectionObserver({
+  //   threshold: 0.5,
+  // });
 
-  useEffect(() => {
-    if (isLogged) {
-      return;
-    }
+  // useEffect(() => {
+  //   if (isLogged) {
+  //     return;
+  //   }
 
-    if (isIntersecting && !viewStartTime) {
-      const currentTime = new Date();
-      setViewStartTime(currentTime);
-    }
+  //   if (isIntersecting && !viewStartTime) {
+  //     const currentTime = new Date();
+  //     setViewStartTime(currentTime);
+  //   }
 
-    if (!isIntersecting && viewStartTime) {
-      const currentTime = new Date();
-      const duration = Math.floor(
-        (currentTime.getTime() - viewStartTime.getTime()) / 1000
-      );
+  //   if (!isIntersecting && viewStartTime) {
+  //     const currentTime = new Date();
+  //     const duration = Math.floor(
+  //       (currentTime.getTime() - viewStartTime.getTime()) / 1000
+  //     );
 
-      const func = async () => {
-        const analytics = await clientAnalytics;
-        if (analytics) {
-          logEvent(analytics, props.eventName, {
-            ...props.context,
-            duration: duration,
-          });
-        }
-      };
+  //     const func = async () => {
+  //       const analytics = await clientAnalytics;
+  //       if (analytics) {
+  //         logEvent(analytics, props.eventName, {
+  //           ...props.context,
+  //           duration: duration,
+  //         });
+  //       }
+  //     };
 
-      func();
-      console.log(
-        `Logging event ${props.eventName}: Component was in view for ${duration} seconds`
-      );
-      setIsLogged(true);
-      setViewStartTime(null);
-    }
-  }, [isIntersecting, isLogged, viewStartTime, props]);
+  //     func();
+  //     console.log(
+  //       `Logging event ${props.eventName}: Component was in view for ${duration} seconds`
+  //     );
+  //     setIsLogged(true);
+  //     setViewStartTime(null);
+  //   }
+  // }, [isIntersecting, isLogged, viewStartTime, props]);
 
-  return <div ref={ref}></div>;
+  return <div></div>;
 }
--- src/components/log-on-view.tsx
+++ src/components/log-on-view.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import {useIntersectionObserver} from "usehooks-ts";
+// import {useIntersectionObserver} from "usehooks-ts";
 import {useEffect, useState} from "react";
 import {logEvent} from "firebase/analytics";
 import {clientAnalytics} from "@/lib/firebase/client-app";
@@ -11,25 +11,25 @@ interface Props {
 }
 export default function LogOnView(props: Props) {
   const [isLogged, setIsLogged] = useState(false);
-  const {isIntersecting, ref} = useIntersectionObserver({
-    threshold: 0.5,
-  });
+  // const {isIntersecting, ref} = useIntersectionObserver({
+  //   threshold: 0.5,
+  // });
 
-  useEffect(() => {
-    if (isIntersecting && !isLogged) {
-      console.log(`Logging event ${props.eventName} on view`);
+  // useEffect(() => {
+  //   if (isIntersecting && !isLogged) {
+  //     console.log(`Logging event ${props.eventName} on view`);
 
-      const func = async () => {
-        const analytics = await clientAnalytics;
-        if (analytics) {
-          logEvent(analytics, props.eventName, props.context);
-        }
-      };
+  //     const func = async () => {
+  //       const analytics = await clientAnalytics;
+  //       if (analytics) {
+  //         logEvent(analytics, props.eventName, props.context);
+  //       }
+  //     };
 
-      func();
-      setIsLogged(true);
-    }
-  }, [isIntersecting, isLogged, props]);
+  //     func();
+  //     setIsLogged(true);
+  //   }
+  // }, [isIntersecting, isLogged, props]);
 
-  return <div ref={ref}></div>;
+  return <div></div>;
 }
