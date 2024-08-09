33a0523d8f56cda84b4e4c0a25565b85c8d6f46d
Joao Jacques <joao.jacques@yumu.ai>
2024-08-09 17:41:23 -0300

    build(deps): update package-lock.json, package.json, page.tsx and tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,6 @@
 import type {Config} from "tailwindcss";
 
+const svgToDataUri = require("mini-svg-data-uri");
 const {
   default: flattenColorPalette,
 } = require("tailwindcss/lib/util/flattenColorPalette");
@@ -82,6 +83,18 @@ const config = {
     require("tailwindcss-animate"),
     require("@tailwindcss/typography"),
     addVariablesForColors,
+    function ({matchUtilities, theme}: any) {
+      matchUtilities(
+        {
+          "bg-grid": (value: any) => ({
+            backgroundImage: `url("${svgToDataUri(
+              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="32" height="32" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`
+            )}")`,
+          }),
+        },
+        {values: flattenColorPalette(theme("backgroundColor")), type: "color"}
+      );
+    },
   ],
 } satisfies Config;
 
